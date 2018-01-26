7de5c1c5ec788892f596be25dd9c71e1856507e9
erickangMSFT <erickangatms@outlook.com>
2018-01-26 11:20:55 -0800

    .

--- cloudera/docker.sh
+++ cloudera/docker.sh
@@ -1,3 +1,3 @@
-docker run --name cloudera --hostname=quickstart.cloudera --privileged=true -d -t -i -p 8888 cloudera/quickstart /usr/bin/docker-quickstart
+docker run --name cloudera --hostname=quickstart.cloudera --privileged=true -d -t -i -p 8888 -p 7180:7180 -p 8080:80 cloudera/quickstart /usr/bin/docker-quickstart
 docker exec -ti cloudera /bin/bash
 /usr/bin/docker-quickstart
